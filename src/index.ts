7127df04bff28f325661bd88271a1da457ef5e2d
Juan Pablo Cafferata <juanpablo.cafferata@gmail.com>
2026-02-04 16:38:02 +0100

    refactor: update locales and defaultLocale to use I18nLocale enum

--- src/index.ts
+++ src/index.ts
@@ -2,16 +2,17 @@ import en from "./messages/en.json";
 import es from "./messages/es.json";
 import pt from "./messages/pt.json";
 
-export const locales = ["en", "pt", "es"] as const;
-export type Locale = typeof locales[number];
-
 export enum I18nLocale {
   EN = "en",
   PT = "pt",
   ES = "es",
 }
 
-export const defaultLocale: Locale = "en";
+export const locales = [I18nLocale.EN, I18nLocale.PT, I18nLocale.ES] as const;
+
+export type Locale = (typeof locales)[number];
+
+export const defaultLocale: Locale = I18nLocale.EN;
 
 export const messages = {
   en,
